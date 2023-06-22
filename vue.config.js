9713a80828eee4c8be584a74de5bd75239edbc25
Anders Gorboe <anders.gorboe@drift.oslo.kommune.no>
2023-06-22 10:23:56 +0200

    trying config for github-pages

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
     }
   },
 
-  publicPath: '/',
+  publicPath: process.env.NODE_ENV === "production" ? "/oceanic-airlines-frontend/" : "/",
 
   transpileDependencies: ['vuetify'],
 
